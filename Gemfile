794cb46adc6d68693a3e7420f5bb2f58b09d0a08
Christopher Lopes <christopher.lopes@gmail.com>
2012-01-28 18:17:29 -0800

    Clean up from when ruby was providing the calendar json file.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,2 @@
 source :gemcutter
 gem 'sinatra', '1.1.0'
-gem 'nokogiri', '1.4.4'
--- app.rb
+++ app.rb
@@ -1,30 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'nokogiri'
-require 'open-uri'
-require 'json'
-require 'date'
 
 get '/' do
 	redirect '/index.html'
-end
-
-get '/jeansday.json' do
-	content_type :json
-
-	doc = Nokogiri::XML(open("http://www.google.com/calendar/feeds/6bkqn7r85goj12qe3ps1e2nlug%40group.calendar.google.com/public/basic")).css("entry")
-	#doc = Nokogiri::XML(open("test.xml")).css("entry")
-
-	entries = Hash.new
-	doc.each do |item|
-		title = item.at_css("title").content
-		content = item.at_css("summary").content.to_s
-		contentStart = content.index('When: ') + 6
-		contentEnd = content.index('<')
-		theDate = content[contentStart,contentEnd - contentStart]
-
-		entries[Date.parse(theDate).to_s] = title
-	end
-
-	return entries.to_json
 end
